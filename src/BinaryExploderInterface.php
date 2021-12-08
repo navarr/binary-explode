e14e5717c12fb7f6187f05fb41c9ed6d60fdcbca
Navarr Barnier <me@navarr.me>
2021-12-08 09:59:58 -0500

    [WIP] Conversion to classes and interface

---
+++ src/BinaryExploderInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Copyright 2021 Navarr Barnier.
+ * Licensed under MIT
+ */
+
+namespace Navarr\BinaryExploder;
+
+ interface BinaryExploderInterface
+ {
+     /**
+      * @return iterable<int> All bit values that make up the passed `$value` (e.g. 3 -> 1 2).  Order is not guaranteed.
+      */
+     public function explode(int $value): iterable;
+ }
